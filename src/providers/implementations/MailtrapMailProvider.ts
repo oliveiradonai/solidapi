ba24ab9437555e3680b0c93ad765b9fcc9274f3e
oliveiradonai <adonai.fig@gmail.com>
2022-08-10 07:52:15 -0300

    init

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,33 @@
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+    private transporter: Mail;
+
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: '',
+            port: 0,
+            auth: {
+                user: '',
+                pass: ''
+            }
+        })
+    }
+
+    async sendMail(message: IMessage): Promise<void> {
+        await this.transporter.sendMail({
+            to: {
+                name: message.to.name,
+                address: message.to.email
+            },
+            from: {
+                name: message.from.name,
+                address: message.from.email
+            },
+            subject: message.subject,
+            html: message.body
+        })
+    }
+}
